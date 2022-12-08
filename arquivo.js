d3e4724ff8e6f500c05f74f2ae81121a3766cfe4
Abrahão Luiz Rigel <116273263+abrahaorigel@users.noreply.github.com>
2022-12-08 10:25:48 -0300

    Criação arquivo
    Código do arquivo.js

---
+++ arquivo.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var url = require('fs');
+var fs = require('url');
+http.createServer(function (req, res){
+    var q = url.parse(req.url, true).query;
+    res.write('Arquivo criado com sucesso!');
+    fs.appendFile(q.nome+'.txt', q.conteudo, function (err) {
+        if (err) throw err;
+      });
+    res.end();
+}).listen(80);
